6ae52afec0a7cef810a70dbdaa5ad0112f695fc0
Garv Bhatia <bhatiagarv07@gmail.com>
2025-12-02 14:51:58 +0530

    Add files via upload

---
+++ 78.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define MAX 10
+int main() {
+    int matrix[MAX][MAX], n, i, j;
+    int sum = 0;
+
+    printf("Enter the size of the square matrix (max %d): ", MAX);
+    scanf("%d", &n);
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        sum += matrix[i][i];
+    }
+
+    printf("The sum of the main diagonal elements is: %d\n", sum);
+
+    return 0;
+}
